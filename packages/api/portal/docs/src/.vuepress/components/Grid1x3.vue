55a948e5bb20391c095e7616dacaeccbd4b364e5
Ubuntu <greykhan@greykhan.5ydeztzxbwgerc1fa5ad5pwy0g.ix.internal.cloudapp.net>
2020-12-11 18:38:50 +0000

    Configured server. Moving development to local environment

---
+++ packages/api/portal/docs/src/.vuepress/components/Grid1x3.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex align-center justify-space-between flex-wrap">
+    <span
+      v-for="gridItem in gridItems"
+      :key="gridItem.title"
+      class="w-full w-1/3"
+    >
+      <div class="flex flex-col align-center">
+        <img class="w-48 h-48 mb-4" :src="gridItem.icon" />
+        <h1 class="text-2xl font-bold mb-4">{{ gridItem.title }}</h1>
+        <p class="text-lg">{{ gridItem.desc }}</p>
+      </div>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    gridItems: Array,
+  },
+};
+</script>
---
+++ packages/api/portal/docs/src/index.md
@@ -0,0 +1,15 @@
+---
+home: true
+heroImage: https://v1.vuepress.vuejs.org/hero.png
+tagline: A web portal for the open gov project that allows you to browse through the open gov ecosystem
+actionText: Quick Start →
+actionLink: /guide/
+features:
+- title: Feature 1 Title
+  details: Feature 1 Description
+- title: Feature 2 Title
+  details: Feature 2 Description
+- title: Feature 3 Title
+  details: Feature 3 Description
+footer: Made by Arsala Grey with ❤️
+---
